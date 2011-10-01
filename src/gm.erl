bdffa2afea3879019b9083132915e279ffee2b78
nuex <nx@nu-ex.com>
2011-10-01 01:20:51 -0400

    implement anti-hacking technology

--- src/gm.erl
+++ src/gm.erl
@@ -25,9 +25,9 @@
 %% Which returns a list of characteristics to be retrived with proplists:get_value
 %%
 identify_explicit(File, Options) ->
-  Template = "identify -format ':format_string' :file",
+  Template = "identify -format :format_string :file",
   TemplateOpts = [{file, File}, {format_string, identify_format_string(Options)}],
-  Result = os:cmd(cmd_string(Template, TemplateOpts)),
+  Result = os:cmd(bind_data(Template, TemplateOpts, [escape])),
   case cmd_error(Result) of
     {error, Msg}  -> {error, Msg};
     no_error      -> parse_identify_explicit(Result)
@@ -57,8 +57,8 @@ mogrify(File, Options) ->
 
 %% Run an os:cmd based on a template and passed in options
 exec_cmd(Template, ExtraOptions, Options) ->
-  OptString = opt_cmd_string(Options),
-  PreParsed = cmd_string(Template, ExtraOptions),
+  OptString = opt_string(Options),
+  PreParsed = bind_data(Template, ExtraOptions, [escape]),
   CmdString = re:replace(PreParsed, "{{options}}", OptString, [{return, list}]),
   Cmd = os:cmd("gm " ++ CmdString),
   parse_result(Cmd).
@@ -96,23 +96,34 @@ converted_value(_Label, V) ->
 
 
 %% Build the option part of the command string from a list of options
-opt_cmd_string(Options) ->
-  opt_cmd_string("", Options).
-opt_cmd_string(Str, []) ->
-  Str;
-opt_cmd_string(CmdStr, [Option|Options]) ->
-  [OptCmdString, Opts] = gm_options:opt(Option),
-  ParsedCommandStr = cmd_string(OptCmdString, Opts),
-  NewCmdStr = string:join([CmdStr, ParsedCommandStr], " "),
-  opt_cmd_string(NewCmdStr, Options).
+opt_string(Options) ->
+  opt_string("", Options).
+opt_string(OptString, []) ->
+  OptString;
+opt_string(OptString, [Option|Options]) ->
+  [Switch, Template, Data] = gm_options:opt(Option),
+  NewOptString = case [Template, Data] of
+    ["", []] ->
+      string:join([OptString, Switch], " ");
+    _ ->
+      Parsed = "'" ++ bind_data(Template, Data, []) ++ "'",
+      string:join([OptString, Switch, Parsed], " ")
+  end,
+  opt_string(NewOptString, Options).
 
-%% Build a command string from a template and a list of options
-cmd_string(Cmd, [{Key, Value}|Rest]) ->
+%% Bind data to a command template
+bind_data(Template, [{Key, Value}|Rest], Options) ->
   Search = ":" ++ atom_to_list(Key),
-  NewCmd = re:replace(Cmd, Search, Value, [{return, list}]),
-  cmd_string(NewCmd, Rest);
-cmd_string(Cmd, []) ->
-  Cmd.
+  Replace = case Options of
+    [escape] ->
+      "'" ++ Value ++ "'";
+    _ ->
+      Value
+  end,
+  NewTemplate = re:replace(Template, Search, Replace, [{return, list}]),
+  bind_data(NewTemplate, Rest, Options);
+bind_data(Template, [], _Options) ->
+  Template.
 
 
 %% Parse an error coming from an executed os:cmd
@@ -120,6 +131,7 @@ cmd_error(Cmd) ->
   R1 = re:run(Cmd, "command not found"),
   R2 = re:run(Cmd, "No such file"),
   R3 = re:run(Cmd, "Request did not return an image"),
+  R4 = re:run(Cmd, "unable to open image"),
   case R1 of
     {match, _} -> {error, command_not_found};
     _ ->
@@ -129,7 +141,10 @@ cmd_error(Cmd) ->
           case R3 of
             {match, _} -> {error, no_image_returned};
             _ ->
-              no_error
+              case R4 of
+                {match, _} -> {error, unable_to_open};
+                _ -> no_error
+              end
           end
       end
   end.
@@ -155,12 +170,19 @@ parse_result(Result) ->
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
-identify_test_() ->
-  [{"Gets explicit image info", fun test_image_info/0}].
+gm_test_() ->
+  [
+    {"Gets explicit image info", fun test_image_info/0},
+    {"Doesn't get hacked", fun test_escapes_hacking/0}
+  ].
 
 test_image_info() ->
   Img = "sandbox/cyberbrain.jpg",
   Info = identify_explicit(Img, [width]),
   ?assertMatch(600, proplists:get_value(width, Info)).
+
+test_escapes_hacking() ->
+  mogrify("baz", [{output_directory, "$(touch hackingz)"}]),
+  ?assertMatch(false, filelib:is_file("hackingz")).
 
 -endif.
--- src/gm_options.erl
+++ src/gm_options.erl
@@ -3,31 +3,31 @@
 
 
 opt({crop, Width, Height}) ->
-  ["-crop :widthx:height", [
+  ["-crop", ":widthx:height", [
     {width, integer_to_list(Width)},
     {height, integer_to_list(Height)}
   ]];
 opt({output_directory, Dir}) ->
-  ["-output-directory :output_directory", [{output_directory, Dir}]];
+  ["-output-directory", ":output_directory", [{output_directory, Dir}]];
 opt(create_directories) ->
-  ["-create-directories", []];
+  ["-create-directories", "", []];
 opt(flip) ->
-  ["-flip", []];
+  ["-flip", "", []];
 opt(magnify) ->
-  ["-magnify", []];
+  ["-magnify", "", []];
 opt({rotate, Degrees}) ->
-  ["-rotate :degrees", [{degrees, integer_to_list(Degrees)}]];
+  ["-rotate", ":degrees", [{degrees, integer_to_list(Degrees)}]];
 opt({blur, Radius, Sigma}) ->
-  ["-blur :radiusx:sigma", [
+  ["-blur", ":radiusx:sigma", [
     {radius, integer_to_list(Radius)},
     {sigma, integer_to_list(Sigma)}
   ]];
 opt({crop, Width, Height, XOffset, YOffset}) ->
-  ["-crop :widthx:height+:x_offset+:y_offset", [
+  ["-crop", ":widthx:height+:x_offset+:y_offset", [
     {width, integer_to_list(Width)},
     {height, integer_to_list(Height)},
     {x_offset, integer_to_list(XOffset)},
     {y_offset, integer_to_list(YOffset)}
   ]];
 opt({edge, Radius}) ->
-  ["-edge :radius", [{radius, integer_to_list(Radius)}]].
+  ["-edge", ":radius", [{radius, integer_to_list(Radius)}]].

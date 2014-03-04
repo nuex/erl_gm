75683e576e1a25b96f59fc3c292e790f986a8c68
nuex <nx@nu-ex.com>
2014-03-04 02:51:49 -0500

    add adjoin option, convert integers elsewhere

--- src/gm_options.erl
+++ src/gm_options.erl
@@ -4,13 +4,13 @@
 
 opt({crop, Width, Height}) ->
   {"-crop", ":widthx:height", [
-    {width, integer_to_list(Width)},
-    {height, integer_to_list(Height)}
+    {width, Width},
+    {height, Height}
   ]};
 opt({resize, Width, Height}) ->
   {"-resize", ":widthx:height", [
-    {width, integer_to_list(Width)},
-    {height, integer_to_list(Height)}
+    {width, Width},
+    {height, Height}
   ]};
 opt({output_directory, Dir}) ->
   {"-output-directory", ":output_directory", [{output_directory, Dir}]};
@@ -21,44 +21,47 @@ opt(flip) ->
 opt(magnify) ->
   {"-magnify"};
 opt({rotate, Degrees}) ->
-  {"-rotate", ":degrees", [{degrees, integer_to_list(Degrees)}]};
+  {"-rotate", ":degrees", [{degrees, Degrees}]};
 opt({blur, Radius, Sigma}) ->
   {"-blur", ":radiusx:sigma", [
-    {radius, integer_to_list(Radius)},
-    {sigma, integer_to_list(Sigma)}
+    {radius, Radius},
+    {sigma, Sigma}
   ]};
 opt({crop, Width, Height, XOffset, YOffset}) ->
   {"-crop", ":widthx:height+:x_offset+:y_offset", [
-    {width, integer_to_list(Width)},
-    {height, integer_to_list(Height)},
-    {x_offset, integer_to_list(XOffset)},
-    {y_offset, integer_to_list(YOffset)}
+    {width, Width},
+    {height, Height},
+    {x_offset, XOffset},
+    {y_offset, YOffset}
   ]};
 opt({edge, Radius}) ->
-  {"-edge", ":radius", [{radius, integer_to_list(Radius)}]};
+  {"-edge", ":radius", [{radius, Radius}]};
 opt({size, Width, Height}) ->
   {"-size", ":widthx:height", [
-    {width, integer_to_list(Width)},
-    {height, integer_to_list(Height)}
+    {width, Width},
+    {height, Height}
   ]};
 opt({thumbnail, Width, Height}) ->
   {"-thumbnail", ":widthx:height^", [
-    {width, integer_to_list(Width)},
-    {height, integer_to_list(Height)}
+    {width, Width},
+    {height, Height}
   ]};
 opt({gravity, Gravity}) ->
   {"-gravity", ":gravity", [{gravity, Gravity}]};
 opt({quality, Quality}) ->
-  {"-quality", ":quality", [{quality, integer_to_list(Quality)}]};
+  {"-quality", ":quality", [{quality, Quality}]};
 opt({extent, Width, Height}) ->
   {"-extent", ":widthx:height", [
-    {width, integer_to_list(Width)},
-    {height, integer_to_list(Height)}
+    {width, Width},
+    {height, Height}
   ]};
 opt({type, Type}) ->
   {"-type", ":type", [{type, Type}]};
 opt({interlace, Interlace}) ->
   {"-interlace", ":interlace", [{interlace, Interlace}]};
 opt({format, Format}) ->
-  {"-format", ":format", [{format, atom_to_list(Format)}]}.
-
+  {"-format", ":format", [{format, Format}]};
+opt(adjoin) ->
+  {"-adjoin"};
+opt('+adjoin') ->
+  {"+adjoin"}.
